83eca140c604e0b9464d6ecbfab5383689190bd2
StefaniaH016 <160367091+StefaniaH016@users.noreply.github.com>
2024-05-07 17:00:43 -0500

    Update Cajero.java

--- supermercado/src/main/java/org/uniquindio/domain/Cajero.java
+++ supermercado/src/main/java/org/uniquindio/domain/Cajero.java
@@ -5,9 +5,15 @@ public class Cajero {
     private String nombre;
     private String identificacion;
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     public String getIdentificacion() {
-        return identificacion;
+        return identificación;
     }
 
     public void setIdentificacion(String identificacion) {
